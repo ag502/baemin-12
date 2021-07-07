f92e21c5316419c49557c2b3284db0ec4ce6042e
cothis <minho1030@naver.com>
2021-07-08 00:28:03 +0900

    Feat : Add Common.js

---
+++ src/script/common.js
@@ -0,0 +1,48 @@
+const debouncer = (callback, delay) => {
+  let timer = undefined;
+  
+  return (...params) => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = undefined;
+    }
+    
+    timer = setTimeout(callback, delay, ...params);
+  }
+}
+
+const validator = () => {
+  const map = new Map();
+
+  const addValidate = (regExp, match, message, $target, $message) => {
+    if (!map.get($target)) {
+      map.set($target, { $message, valids: [] });
+    }
+    
+    map.get($target).valids.push({ regExp, match, message });
+  };
+  
+  const checkValid = ({target}) => {
+    const $input = target.closest('.input-field > input')
+    const { $message, valids} = map.get($input);
+    if (valids) {
+      const value = $input.value;
+
+      const invalid = valids.find(valid => {
+        const regExp = new RegExp(valid.regExp);
+        return regExp.test(value) !== valid.match;
+      });
+
+      if (invalid) {
+        $message.textContent = invalid.message;
+        $message.style = 'display: block';
+      } else {
+        $message.style = '';
+      }
+    }
+  }
+
+  return { addValidate, checkValid };
+};
+
+export {debouncer, validator};
--- src/script/join_certification.js
+++ src/script/join_certification.js
@@ -1,15 +1,12 @@
+import { debouncer, validator } from './common.js'
+
 document.addEventListener('DOMContentLoaded', () => {
-  const validator = () => {};
-
-  const valids = new Map();
-
-  const addValidate = (regExp, match, message, $target, $message) => {
-    if (!valids.get($target)) {
-      valids.set($target, []);
-    }
-
-    valids.get($target).push({ regExp, match, message, $message });
-  };
-
+  const { addValidate, checkValid } = validator();
+  
   const $phone = document.querySelector('#phone');
+  const $message = $phone.parentElement.parentElement.children[2];
+  addValidate('000-000', true, '전화번호를 제대로 입력해주세요', $phone, $message);
+
+  const debouncedValidator = debouncer(checkValid, 250);
+  document.querySelector('.join-form').addEventListener('keyup', debouncedValidator);
 });
