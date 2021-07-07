5dac0419438fd89604e2d96cbaf64d2355a85ced
LeeJaeyun <ag502@naver.com>
2021-07-07 10:49:03 +0900

    Style : add common padding layout

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-const pageRouter = require('./routers/main');
+const mainRouter = require('./routers/main');
 const joinAgreeRouter = require('./routers/join_agree');
+const loginRouter = require('./routers/login');
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
 app.use('/assets', express.static(path.join(__dirname, '../assets/')));
 app.use('/node_modules', express.static(path.join(__dirname, `../node_modules/@fortawesome/fontawesome-free/`)));
-app.use('/styles', express.static(`${__dirname}/styles`)); 
+app.use('/styles', express.static(`${__dirname}/styles`));
 app.use('/script', express.static(`${__dirname}/script`));
-app.use(pageRouter);
-app.use(joinAgreeRouter)
+
+app.use(mainRouter);
+app.use(joinAgreeRouter);
+app.use(loginRouter);
 
 app.listen(port, () => {
   console.log(`${port} is listening`);
