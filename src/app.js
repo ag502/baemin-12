d2abaae60029757c575d500e2910a5fc26a86c97
LeeJaeyun <ag502@naver.com>
2021-07-07 14:49:24 +0900

    Feat : apply session login

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 const port = 5000;
 
@@ -10,6 +12,15 @@ const loginRouter = require('./routers/login');
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
+app.use(
+  session({
+    secret: '#KFg$g!$KT9U}BY',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/assets', express.static(path.join(__dirname, '../assets/')));
 app.use('/node_modules', express.static(path.join(__dirname, `../node_modules/@fortawesome/fontawesome-free/`)));
 app.use('/styles', express.static(`${__dirname}/styles`));
--- src/routers/login.js
+++ src/routers/login.js
@@ -3,9 +3,27 @@ const loginRouter = express.Router();
 
 loginRouter.get('/login', async (req, res) => {
   try {
+    if (req.session.id) {
+      console.log(req.session.id);
+      req.session.destroy();
+    }
     res.render('login', { title: '로그인' });
   } catch (error) {
     console.log(error);
+  }
+});
+
+const dummyAcount = { id: 'ag502', password: '123' };
+
+loginRouter.post('/login', async (req, res) => {
+  const { id, password } = req.body;
+  if (id === dummyAcount.id && password === dummyAcount.password) {
+    req.session.id = id;
+    req.session.save(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.send();
   }
 });
 
